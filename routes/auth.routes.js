7287d7bc8c70d2a03ec59de143e3c4380a31068c
Aaditya <uaaditya759@gmail.com>
2021-03-28 13:51:30 +0530

    Sprint 1 Commit: Added route for student login

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -13,19 +13,51 @@ const { check, validationResult }=require('express-validator');
 // Sign-in student
 router.post('/signin-student', (req,res,next) => {
     let getUser;
-    studentSchema.findOne().then().then().catch();
+    studentSchema.findOne({ rollno: req.body.idno })
+    .then((user) => {
+        if(!user) {
+            return res.status(401).json({
+                message: 'Authentication failed',
+            });
+        }
+        getUser=user;
+        return bcrypt.compare(req.body.password,user.password);
+    }).then((response) => {
+        if(!response) {
+            return res.status(401).json({
+                message: 'Authentication failed',
+            });
+        }
+        let jwtToken=jwt.sign({
+            rollno: getUser.rollno,
+            userID: getUser._id,
+        },
+        'secret',
+        {
+            expiresIn: '1h',
+        });
+        res.status(200).json({
+            token=jwtToken,
+            expiresIn: 3600,
+            msg: getUser,
+        });
+    }).catch((err) => {
+        return res.status(401).json({
+            message: 'Authentication failed',
+        });
+    });
 });
 
 // Sign-in faculty
 router.post('/signin-faculty', (req,res,next) => {
     let getUser;
-    facultySchema.findOne().then().then().catch();
+    facultySchema.findOne({ idno: req.body.idno }).then().then().catch();
 });
 
 // Sign-in chair
 router.post('/signin-chair', (req,res,next) => {
     let getUser;
-    chairSchema.findOne().then().then().catch();
+    chairSchema.findOne({ idno: req.body.idno }).then().then().catch();
 });
 
 
