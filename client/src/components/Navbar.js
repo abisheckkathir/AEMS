346dedbd5522397d74f962ddc9feb8aa1379b3a9
dhanushcrueiso <dhanushravula@gmail.com>
2021-04-04 12:39:47 +0530

    changes css for buttons and animations added

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -2,41 +2,50 @@ import React, { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { logout } from '../actions/action.auth';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Button from 'react-bootstrap/Button';
+
 
 const Navbar = ({ isAuthenticated, logout }) => {
   const authorizedLinks = (
     <>
+    <Button marginright="20px" variant="light"  >
     <li>
         <Link to="/users">Courses</Link>
       </li>
+      </Button>
+      <Button marginright="20px" variant="light"  >
       <li>
         <Link to="/" onClick={logout}>
           Logout
         </Link>
       </li>
-      
+      </Button>
     </>
   );
 
   const guestLinks = (
     <Fragment>
+      <Button marginright="20px" variant="light"  >
       <li>
         <Link to="/login">Log In</Link>
       </li>
+      </Button>
+      <Button marginright="20px" variant="light"  >
       <li>
         <Link to="/signup">Sign Up</Link>
       </li>
-      
+      </Button>
     </Fragment>
   );
 
   return (
     <div>
       <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
+        <ul><Button marginright="20px" variant="light"  >
+          <li >
+            <Link to="/">Home </Link>
+          </li></Button>
           {
             <Fragment>
               {isAuthenticated ? authorizedLinks : guestLinks}
--- client/src/pages/Sample.js
+++ client/src/pages/Sample.js
@@ -1,7 +1,11 @@
 import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Sample = () => {
-  return <div>Sample Private Route</div>;
+  return  <div>Sample private route</div>
+
+  
+    
 };
 
 export default Sample;
